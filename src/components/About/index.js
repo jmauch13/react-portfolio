49a3d8a0887c59a1b7c1abc8a2b7b7ff4b2ed58c
Jennie Mauch <jenniemauch@me.com>
2022-07-09 15:55:48 -0400

    redid portfolio pages

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,6 +14,9 @@ function About() {
 				<p>
 					I am a full stack web developer looking to build a more
 					intuitive user experience on the web.
+					I feel very strongly about accessibility, as I have worked for the deaf and hard of hearing community 
+					providing closed captioning in a variety of settings for many years.
+        I			would like to focus my career in web developement around improving accessibility.
 				</p>
 			</div>
 		</section>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import zenImage from "../../assets/zen.png";
 import quizImage from "../../assets/quiz.png";
 import weatherImage from "../../assets/weather.png";
@@ -12,7 +12,8 @@ const portfolioPages = [
     },
     { 
         name: 'Upcycle', 
-        image: upcycleImage
+        image: upcycleImage,
+    
     },
     
     { 
@@ -37,13 +38,13 @@ function Portfolio() {
     
     <div>
         <section>
-            <h1 id="portfolio">Portfolio</h1>
+            <h1 className="page-header">Portfolio</h1>
         </section>
 
-        <div className="page-header">
+        <div className="portfolio-page">
             {portfolioPages.map((portfolioPage) => (
                 <div className="portfolio-image">
-                    <img src={portfolioPage.image} alt="Portfolio item preview screencap." />
+                    <img src={portfolioPage.image} style={{ width: "50%", height: "30%"}} alt="Portfolio item preview screencap." />
                     <p className="header-format">{portfolioPage.name}</p>
                 </div>
             ))}
@@ -51,5 +52,35 @@ function Portfolio() {
     </div>
   );
 }
+
+export default Portfolio;*/
+
+import React from 'react'; 
+import ProjectCards from '../../components/Project'
+import portfolio from '../../portfolio.json'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Wrapper(props) {
+    return <div className="wrapper">{props.children}</div>;
+}
+
+function Portfolio () {  
+
+    return (
+        <section className="container">
+          <div className="project">
+            <h2 className="page-header">Portfolio</h2>
+            <hr></hr>
+          </div>
+
+          <Wrapper id="card-data">
+            {portfolio.map((project) => (
+              <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} topics={project.topics}/>
+            ))}
+          </Wrapper>
+        </section>
+    
+    );
+  }
 
 export default Portfolio;
